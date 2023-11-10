420f6744a2a7c328bf92fb8df917ba6a092c6379
bourne <xuhui.yon@gmail.com>
2023-11-10 12:31:01 +0800

    react18 build

--- config/paths.js
+++ config/paths.js
@@ -21,7 +21,7 @@ const publicUrlOrPath = getPublicUrlOrPath(
   process.env.PUBLIC_URL
 );
 
-const buildPath = process.env.BUILD_PATH || 'build';
+const buildPath = process.env.BUILD_PATH || 'react18';
 
 const moduleFileExtensions = [
   'web.mjs',
